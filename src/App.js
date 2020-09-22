b5dd8d4d91efe9b54b88462ae12a3ae493dc96dd
hyeriful <dlgpfl2523@gmail.com>
2020-09-22 17:31:01 +0900

    moive app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function App() {
-  return <div className="App">Hello</div>;
+function App(props) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    ); // yts.lt/api#list+movies
+    // console.log(movies);
+    setMovies(movies);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map((movie) => {
+            // console.log(movie);
+            return (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            );
+          })}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from "react"; //component를 작성할 때마다 해주어야 함
 import ReactDOM from "react-dom";
 import App from "./App";
 
-//React.StriceMode 문제가 발생하면, 발생된 에러메세지를 바로 출력하는 모드
+//component는 html를 반환하는 함수
+
+// react application은 한 번에 하나의 component만 redering할 수 있다
 ReactDOM.render(
+  //React.StriceMode 문제가 발생하면, 발생된 에러메세지를 바로 출력하는 모드
   <React.StrictMode>
     <App />
   </React.StrictMode>,
