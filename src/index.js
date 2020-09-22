df5a8f7fd21215d2963acaaf7c9d62052322c579
hyeriful <dlgpfl2523@gmail.com>
2020-09-22 17:32:09 +0900

    주석 오타 수정

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 // react application은 한 번에 하나의 component만 redering할 수 있다
 ReactDOM.render(
-  //React.StriceMode 문제가 발생하면, 발생된 에러메세지를 바로 출력하는 모드
+  //React.StrictMode 문제가 발생하면, 발생된 에러메세지를 바로 출력하는 모드
   <React.StrictMode>
     <App />
   </React.StrictMode>,
