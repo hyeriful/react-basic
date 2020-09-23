7a864939f32ceb7ee3944b3cd93a57902535382b
hyeriful <dlgpfl2523@gmail.com>
2020-09-23 23:45:15 +0900

    movie detail page

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import "./Details.css";
 
 class Detail extends React.Component {
   // 2. componentDidMount 됨
@@ -13,7 +14,24 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      const movie = location.state;
+      return (
+        <div className="movie_detail">
+          <img src={movie.poster} alt={movie.title} title={movie.title} />
+          <div className="movie__data">
+            <h3 className="movie__title">{movie.title}</h3>
+            <h5 className="movie__year">{movie.year}</h5>
+            <ul className="movie__genres">
+              {movie.genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="movie__summary">{movie.summary}...</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
